d64c74ffb296093b533c43f966041e68951d4107
Farman <farmanbehlim1@gmail.com>
2024-01-04 17:58:31 +0530

    first commit

---
+++ myapp/src/component/Game.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import Gameobj from './Gamedata';
+import bell from "../assets/bell.wav"
+import "./Game.css"
+function Game() {
+    const [selectedId, setSelectedId] = useState(null);
+    const playSound = () => {
+        const audio = new Audio(bell);
+        audio.play();
+    };
+   
+    console.log(selectedId)
+    return (
+        <div>
+            {selectedId !== null && (
+                <div className='gif'>
+                    
+                    <video key={selectedId} width="640" height="360" autoPlay muted>
+                        
+                        <source src={Gameobj.find((data) => data.id === selectedId).gif} />
+                        
+                    </video>
+                   
+                </div>
+            )}
+            <div className='buttongrid'>
+
+                {Gameobj.map((data) => (
+
+                    <div key={data.id} >
+                       
+                        <div key={data.id} className='chilld'>
+                        <button onClick={() => { setSelectedId(data.id); playSound(); }}>
+                            {data.number}</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Game;
---
+++ myapp/src/component/Gamedata.jsx
@@ -0,0 +1,72 @@
+import gif0 from "../assets/gif0.mp4"
+import gif1 from "../assets/gif1.mp4"
+import gif2 from "../assets/gif2.mp4"
+import gif3 from "../assets/gif3.mp4"
+import gif4 from "../assets/gif4.mp4"
+import gif5 from "../assets/gif5.mp4"
+import gif6 from "../assets/gif6.mp4"
+import gif7 from "../assets/gif7.mp4"
+import gif8 from "../assets/gif8.mp4"
+import gif9 from "../assets/gif9.mp4";
+const Gameobj = [
+    {
+        number: 0,
+        gif: gif0,
+        id: 0
+    },
+    {
+        number: 1,
+        image: "wow1",
+        gif: gif1,
+        id: 1
+    },
+    {
+        number: 2,
+        image: "wow2",
+        gif: gif2,
+        id: 2
+    },
+    {
+        number: 3,
+        image: "wow3",
+        gif: gif3,
+        id: 3
+    },
+    {
+        number: 4,
+        image: "wow4",
+        gif: gif4,
+        id: 4
+    },
+    {
+        number: 5,
+        image: "wow5",
+        gif: gif5,
+        id: 5
+    },
+    {
+        number: 6,
+        image: "wow6",
+        gif: gif6,
+        id: 6
+    },
+    {
+        number: 7,
+        image: "wow7",
+        gif: gif7,
+        id: 7
+    },
+    {
+        number: 8,
+        image: "wow8",
+        gif: gif8,
+        id: 8
+    },
+    {
+        number: 9,
+        image: "wow9",
+        gif: gif9,
+        id: 9
+    },
+]
+export default Gameobj
