097bb81ab39c6b5d80efca91e8bb60bd6fa76840
Farman <farmanbehlim1@gmail.com>
2024-01-06 17:52:22 +0530

    first commit

--- myapp/src/component/Game.jsx
+++ myapp/src/component/Game.jsx
@@ -1,43 +1,52 @@
-import React, { useEffect, useState } from 'react';
+
+import React, { useState } from 'react';
 import Gameobj from './Gamedata';
-import bell from "../assets/bell.wav"
-import "./Game.css"
+import bell from "../assets/bell.wav";
+import "./Game.css";
+
 function Game() {
-    const [selectedId, setSelectedId] = useState(null);
-    const playSound = () => {
-        const audio = new Audio(bell);
-        audio.play();
-    };
-   
-    console.log(selectedId)
-    return (
-        <div className='maincontainer'>
-            {selectedId !== null && (
-                <div className='gif'>
-                    
-                    <video key={selectedId}  autoPlay muted>
-                        
-                        <source src={Gameobj.find((data) => data.id === selectedId).gif} />
-                        
-                    </video>
-                   
-                </div>
-            )}
-            <div className='buttongrid'>
+  const [selectedId, setSelectedId] = useState(null);
+  const [empty, setEmptyDiv] = useState(null);
 
-                {Gameobj.map((data) => (
+  const playSound = () => {
+    const audio = new Audio(bell);
+    audio.play();
+  };
 
-                    <div key={data.id} className='chilld'>
-                       
-                       
-                        <button className='number' onClick={() => { setSelectedId(data.id); playSound(); }}>
-                            {data.number}</button>
-                        
-                    </div>
-                ))} 
-            </div>
-        </div> 
-    );
+  console.log(selectedId);
+
+  return (
+    <div className='maincontainer'>
+      <div className='childbox1'>
+        {selectedId == null ? (
+          <div className='start'>Click the button and see the magic</div>
+        ) : (
+          <div className='gif'>
+            <video key={selectedId} autoPlay muted>
+     
+              <source src={ Gameobj.find((data) => data.id === selectedId).gif } />
+            </video>
+          </div>
+        )}
+      </div>
+      <div className='buttongrid'>
+        {Gameobj.map((data) => (
+          <div key={data.id} className='chilld'>
+            <button
+              className='number'
+              onClick={() => {
+                setSelectedId(data.id);
+                setEmptyDiv(!null); 
+                playSound();
+              }}
+            >
+              {data.number}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div> 
+  );
 }
 
 export default Game;
